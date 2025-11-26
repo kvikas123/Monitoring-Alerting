4c0a05e7c88994a9ad046627f267582a710cfa3c
kvikas123 <vikas.inetsolv@gmail.com>
2025-11-26 23:56:16 +0530

    Update grafna-promethous.sh

--- grafna-promethous.sh
+++ grafna-promethous.sh
@@ -137,7 +137,7 @@ route:
 receivers:
   - name: pagerduty
     pagerduty_configs:
-      - routing_key: "4bf6bcd8cb9b4607d047c4b6c2d333d6" #add key pagerduty 
+      - routing_key: "6bbf05661b564604c0e69b2427c683af" #add key pagerduty 
         severity: "critical"
 EOF
 
